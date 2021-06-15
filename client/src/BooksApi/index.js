3b86781941681ccc9d61c176aa4b92fa3a587a2c
aschaley ascheneke <schaley1@gmail.com>
2021-06-15 15:02:05 +0300

    change api call to https

--- client/src/BooksApi/index.js
+++ client/src/BooksApi/index.js
@@ -1,8 +1,8 @@
 const getApiSearchUrl = (searchMethod, searchTerm) =>
-  `http://openlibrary.org/search.json?${searchMethod}=${searchTerm}`;
+  `https://openlibrary.org/search.json?${searchMethod}=${searchTerm}`;
 
 export const getBookCoverByOLID = (olid) =>
-  `http://covers.openlibrary.org/b/olid/${olid}-M.jpg`;
+  `https://covers.openlibrary.org/b/olid/${olid}-M.jpg`;
 
 export const noCoverAvaillable =
   "https://cdn.shoplightspeed.com/shops/608154/files/22146758/image.jpg";
